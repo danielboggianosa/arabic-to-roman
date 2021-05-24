646052b14713873dbd2ab996e652a569403e8a72
Daniel Boggiano <dboggiano@indracompany.com>
2021-05-23 21:23:11 -0500

    ready

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = transform = (arabic)=>{
+
+    const units = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    const thousands = ["", "M", "MM", "MMM"]
+
+    const m = Math.floor(arabic / 1000)
+    const cm = arabic % 1000
+    const c = Math.floor(cm / 100)
+    const xc = cm % 100
+    const x = Math.floor(xc / 10)
+    const i = xc % 10
+
+    var roman = thousands[m] + hundreds[c] + tens[x] + units[i]
+
+    return roman
+}
